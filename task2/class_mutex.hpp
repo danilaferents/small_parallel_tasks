bcebd8d5ae7133b26c678f2216712374d716c6f1
Danila Ferents <iambpev@gmail.com>
2019-04-15 12:07:34 +0300

    linux

--- task2/class_mutex.hpp
+++ task2/class_mutex.hpp
@@ -5,8 +5,6 @@
 #include <cstdlib>
 #include <ctime>
 #include <chrono>
-#include <algorithm>
-#include<functional>
 
 std::mutex mutextask2;
 std::vector<std::mutex> mutexvector(10);
@@ -21,17 +19,23 @@ class MassiveTryMutex
 		// MassiveFirstTask& operator=(MassiveFirstTask const&) = delete;
 		void SummFirstTask(int a, int b, int c)
 		{
-			std::cout<<races[a] + races[b] + races[c]<<" ";
+			races[a]=races[a] + races[b] + races[c];
+			races[b]=races[a] + races[b] + races[c];
+			races[c]=races[a] + races[b] + races[c];
 		}
 		void SummSecondTask(int a, int b, int c)
 		{
 			std::lock_guard<std::mutex> lock(mutextask2);
-			std::cout<<races[a] + races[b] + races[c]<<" ";
+			races[a]=races[a] + races[b] + races[c];
+			races[b]=races[a] + races[b] + races[c];
+			races[c]=races[a] + races[b] + races[c];
 		}
 		void SummThirdTask(int a, int b, int c)
 		{
 			while(!std::try_lock(mutexvector[a], mutexvector[b], mutexvector[c]));
-			std::cout<<races[a] + races[b] + races[c]<<" ";
+			races[a]=races[a] + races[b] + races[c];
+			races[b]=races[a] + races[b] + races[c];
+			races[c]=races[a] + races[b] + races[c];
 			mutexvector[a].unlock();
 			mutexvector[b].unlock();
 			mutexvector[c].unlock();
@@ -45,6 +49,8 @@ class MassiveTryMutex
 			std::scoped_lock lock_a(mutexvector[a]);
 			std::scoped_lock lock_b(mutexvector[b]);
 			std::scoped_lock lock_c(mutexvector[c]);
-			std::cout<<races[a] + races[b] + races[c]<<" ";
+			races[a]=races[a] + races[b] + races[c];
+			races[b]=races[a] + races[b] + races[c];
+			races[c]=races[a] + races[b] + races[c];
 		}
 };
